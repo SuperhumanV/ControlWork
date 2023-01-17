f2a61f80acb048517cca2d1c8ca6dc0ef60b540d
Superhuman <poriata@yandex.ru>
2023-01-17 21:31:23 +0300

    Void GetArrEnd

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
 ﻿string[] beginArray = new string[5]
 {'321','qwerty','Ubuntu','Debian','SentOS'};
 string[] endArray = new
+string[beginArray.Length];
 
+void GetArrEnd(string[]
+beginArray,string[] endArray)
+{
+    int count = 0;
+    for(int i = 0; i< beginArray.Length;i++)
+    {
+        if (beginArray[i].Length<=3)
+        {
+            endArray[count] = beginArray[i];
+        }
+    }
+}

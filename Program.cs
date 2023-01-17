fff9caeae3cf2ecdd06867e9b9d4d2e6c95e5940
Superhuman <poriata@yandex.ru>
2023-01-17 21:36:01 +0300

    Void Print Array

--- Program.cs
+++ Program.cs
@@ -16,3 +16,10 @@ beginArray,string[] endArray)
         }
     }
 }
+
+void PrintArray(string[] array)
+{
+    for(int i =0; i< array.Length; i++)
+    Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
